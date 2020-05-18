613f415ba8a9bb9d74cef6f4c2757d30e21d86ad
Nguyễn Quang Vinh <31350719+NguyenQuangVinh123@users.noreply.github.com>
2020-05-18 23:20:27 +0700

    collapseRAdio

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -3,21 +3,31 @@ import { connect } from 'react-redux';
 import PageTop from '../utils/page_top';
 import { getBrands, getWoods } from '../../actions/products_actions';
 import CollapseCheckbox from '../utils/collapseCheckbox';
-
+import {frets,price} from '../utils/Form/Fixed_categories';
+import CollapseRadio from '../utils/collapseRadio';
 const Shop = (props) => {
-    // const [productsBrands,setProductsBrands] = useState(props.products.brands)
+    const [layout,setLayout]  = useState({grid : '',limit : 6,skip : 0,filters : {
+        brand : [],
+        frets: [],
+        wood : [],
+        price : []
+    } })
     useEffect(() => {
         props.dispatch(getBrands());
         props.dispatch(getWoods());
 
     },[])
 
-    // const productBrands = props.products.brands;
     const handleFilters = (filters,cate) =>{
-        console.log(filters)
+       const newFilter = {...layout.filters}
+       newFilter[cate] = filters;
+       setLayout((preState) => ({
+        ...preState,
+        filters : newFilter
+       }))
+      
     }
     return (
-        
         <div>
             <PageTop title="Browse Products" />
             <div className="container">
@@ -25,8 +35,12 @@ const Shop = (props) => {
                     <div className="left">
                         <CollapseCheckbox initState = {true} title='Brands' 
                         list={props.products.brands} handleFilters={((filters) =>handleFilters(filters,'brand')  )}  />
-                        {/* <CollapseCheckbox initState = {true} title='Frets' 
-                        list={props.products.brands} handleFilters={((filters) =>handleFilters(filters,'frets')  )}  /> */}
+                        <CollapseCheckbox initState = {false} title='Frets' 
+                        list={frets} handleFilters={((filters) =>handleFilters(filters,'frets')  )}  />
+                        <CollapseCheckbox initState = {true} title='Wood' 
+                        list={props.products.woods} handleFilters={((filters) =>handleFilters(filters,'wood')  )}  />
+                         <CollapseRadio initState = {true} title='Price' 
+                        list={price} handleFilters={((filters) =>handleFilters(filters,'price')  )}  />
                     </div>
                     <div className="right">
                         right
---
+++ client/src/components/utils/collapseRadio.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import  faAngleDown from '@fortawesome/fontawesome-free-solid/faAngleDown';
+import  faAngleUp from '@fortawesome/fontawesome-free-solid/faAngleUp';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Collapse from '@material-ui/core/Collapse';
+import RadioGroup from '@material-ui/core/RadioGroup'
+import Radio from '@material-ui/core/Radio'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+const CollapseRadio = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CollapseRadio;
